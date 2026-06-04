aae7cfff3c6c9fdc1e0335422ec8894afdef41fb
hnwyllmm <hnwyllmm@users.noreply.github.com>
2026-06-04 11:42:53 +0800

    chore: sync internal file changes
      - deps/oblib/src/lib/thread/thread.cpp
      - src/objit/CMakeLists.txt

--- deps/oblib/src/lib/thread/thread.cpp
+++ deps/oblib/src/lib/thread/thread.cpp
@@ -207,7 +207,7 @@ void Thread::stop()
 uint64_t Thread::get_tenant_id() const
 {
   uint64_t tenant_id = OB_SERVER_TENANT_ID;
-  IRunWrapper *run_wrapper_ = threads_->get_run_wrapper();
+  IRunWrapper *run_wrapper_ = threads_->get_effective_run_wrapper();
   if (OB_NOT_NULL(run_wrapper_)) {
     tenant_id = run_wrapper_->id();
   }
@@ -219,7 +219,7 @@ void Thread::run()
   if (OB_NUMA_SHARED_INDEX != numa_node_) {
     AFFINITY_CTRL.thread_bind_to_node(numa_node_);
   }
-  IRunWrapper *run_wrapper_ = threads_->get_run_wrapper();
+  IRunWrapper *run_wrapper_ = threads_->get_effective_run_wrapper();
   if (OB_NOT_NULL(run_wrapper_)) {
     {
       ObDisableDiagnoseGuard disable_guard;
@@ -364,13 +364,15 @@ void Thread::destroy_stack()
 {
 #ifdef _WIN32
   pth_ = pthread_null();
-#elif !defined(OB_USE_ASAN)
+#else
+#if !defined(OB_USE_ASAN)
   if (stack_addr_ != nullptr) {
     g_stack_allocer.dealloc(stack_addr_);
     stack_addr_ = nullptr;
   }
 #endif
   pth_ = 0;
+#endif
 }
 
 void* Thread::__th_start(void *arg)
