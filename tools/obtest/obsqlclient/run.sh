4cb6fb268c21142746376f67675b0b55680932b9
obdev <obdev@oceanbase.com>
2026-08-07 12:18:32 +0000

    build: migrate SeekDB to native Bazel modules
    Co-authored-by: footka <672528926@qq.com>

---
+++ tools/obtest/obsqlclient/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export OB_SQL_CONFIG=/home/yiming.czw/obtest/obsqlclient/libobsql.conf
+export LD_PRELOAD=/home/yiming.czw/obtest/obsqlclient/libobsql.so.0.0.0
+./obsqltest -t $1
