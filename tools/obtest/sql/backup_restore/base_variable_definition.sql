977053bf16296429bab7b72522b2618e795ba6ae
obdev <obdev@oceanbase.com>
2026-08-25 15:19:47 +0000

    revert strip2ce
    Co-authored-by: footka <672528926@qq.com>
    Co-authored-by: LINxiansheng <wangzelin19961202@gmail.com>

--- tools/obtest/sql/backup_restore/base_variable_definition.sql
+++
@@ -1,3 +0,0 @@
-set global query_cache_size  = 1900917;
-set global sql_select_limit  = 9009172085900917;
-set global wait_timeout  = 900917;
--- tools/obtest/tools/netf_recover.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sleep $1 && sudo tc qdisc del dev $2 root > /dev/null 2>&1 &
